ea302cc0310f407071002064d54f3319130b837d
Zahra-MASHHADI <zahraa.mashadii@gmail.com>
2025-03-15 09:58:03 +0330

    z

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="mainPage">
+
+    <HelloWorld/>
+
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+
+.mainPage{
+  background-color: black;
+  height:100vh;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+    coming soon
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .hello{
+    color:rgb(160, 2, 2) ;
+    font-size: 60px;
+    display: flex;
+    justify-items: center;
+    align-items: center;
+    inset:0;
+    
+  }
+</style>
